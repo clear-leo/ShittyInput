44414dba70591a1e03fca48623aa70d06440cb88
clearleo <g567zp@gmail.com>
2024-08-24 13:11:05 -0500

    Miscellaneous spelling fixes

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@ use std::{io, num::ParseIntError};
 
 pub fn int() -> Result<isize, ParseIntError> {
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Error on terminal input (ShittyInput crate!");
+    io::stdin().read_line(&mut input).expect("Error on terminal input (ShittyInput crate!)");
     let result = match input.trim().parse::<isize>() {
         Ok(result) => Ok(result),
         Err(error) => Err(error)
@@ -28,7 +28,7 @@ pub fn int() -> Result<isize, ParseIntError> {
 
 pub fn string() -> String {
     let mut result = String::new();
-    io::stdin().read_line(&mut result).expect("Error on terminal input (ShittyInput crate!");
+    io::stdin().read_line(&mut result).expect("Error on terminal input (ShittyInput crate!)");
     let result = result.trim();
     result.to_string()
 }
@@ -40,7 +40,7 @@ pub struct float {
 impl float {
     pub fn f32() -> Result<f32, std::num::ParseFloatError> {
         let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Error on terminal input (ShittyInput crate!");
+        io::stdin().read_line(&mut input).expect("Error on terminal input (ShittyInput crate!)");
         let result = match input.trim().parse::<f32>() {
             Ok(result) => Ok(result),
             Err(error) => Err(error)
@@ -50,7 +50,7 @@ impl float {
 
     pub fn f64() -> Result<f64, std::num::ParseFloatError> {
         let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Error on terminal input (ShittyInput crate!");
+        io::stdin().read_line(&mut input).expect("Error on terminal input (ShittyInput crate!)");
         let result = match input.trim().parse::<f64>() {
             Ok(result) => Ok(result),
             Err(error) => Err(error)
